f585edb075fcf7f5f73d0877f24e6734211eda4d
arunyagoojar <arunya.goojar@s.amity.edu>
2024-05-31 12:32:02 +0530

    update

--- Zen Sounds/Zen_SoundsApp.swift
+++ Zen Sounds/Zen_SoundsApp.swift
@@ -6,22 +6,12 @@
 //
 
 import SwiftUI
-import Sparkle
 
 @main
 struct Zen_SoundsApp: App {
-    private let updaterController: SPUStandardUpdaterController
-
-        init() {
-            updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
-        }
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }.commands {
-            CommandGroup(after: .appInfo) {
-                CheckForUpdatesView(updater: updaterController.updater)
-            }
         }
     }
 }
